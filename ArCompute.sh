c680f8f17bfc81a0bc246f897fa992963476ab6c
saitarun135 <saitarun135@gm.com>
2021-03-01 15:59:40 +0530

    read the values dictionary into an array

--- ArCompute.sh
+++ ArCompute.sh
@@ -1,15 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "enter three inputs :: "
 read a b c
-result1=$(($a+$b*$c))
-result2=$(($a*$b+$c))
+result1=$(($a+($b*$c)))
+result2=$((($a*$b)+$c))
 result3=$(($c+($a/$b)))
 result4=$((($a%$b)+$c))
 
 declare -A dictionary
-dictionary[uc2]="$result1"
-dictionary[uc3]="$result2"
-dictionary[uc4]="$result3"
-dictionary[uc5]="$result4"
-echo "${dictionary[@]}"
+	dictionary[uc2]="$result1"
+	dictionary[uc3]="$result2"
+	dictionary[uc4]="$result3"
+	dictionary[uc5]="$result4"
+
+declare -a Array
+	for ((count=0; count<=${#dictionary[*]}; count++))
+		do
+	Array[count]=${dictionary[uc$((count+1))]}
+		done
+			echo "${Array[@]}"
+         
+
