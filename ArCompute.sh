40fa025c502aa4818457afb7c8f5cdc927a1ed28
saitarun135 <saitarun135@gm.com>
2021-03-01 16:21:59 +0530

    Descing order of an array

--- ArCompute.sh
+++ ArCompute.sh
@@ -18,6 +18,19 @@ declare -a Array
 		do
 	Array[count]=${dictionary[uc$((count+1))]}
 		done
-			echo "${Array[@]}"
-         
+			echo "resulted-Array:" ${Array[@]}
 
+length="${#Array[@]}"
+for ((i=0; i<$length; i++))
+do
+   for ((j=$i; j<$length; j++))
+do
+if [[ ${Array[j]} -gt ${Array[i]} ]]
+then
+   temp=${Array[i]}
+   Array[i]=${Array[j]}
+   Array[j]=$temp
+fi
+done
+done
+echo "sorted-Array(descending order):" ${Array[@]}
